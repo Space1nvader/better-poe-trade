43a1d4ff35eb2f821d895043566a88d0b5adb2ae
Pavel Gorin <buteafulspace@gmail.com>
2026-04-20 11:53:15 +0400

    Ну допустим инит

---
+++ scripts/content.js
@@ -0,0 +1,337 @@
+const svgsaveicon = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" viewBox="0 0 512 512" xml:space="preserve">
+<path d="M503.467,0H8.533C3.84,0,0,3.84,0,8.533v392.533c0,2.389,1.024,4.693,2.731,6.229l110.933,102.4  c1.621,1.536,3.669,2.304,5.803,2.304h384c4.693,0,8.533-3.84,8.533-8.533V8.533C512,3.84,508.16,0,503.467,0z M93.867,17.067  h324.267V192l-16.299,12.288l-12.117,9.045H122.283L93.867,192V17.067z M494.933,494.933H122.795L17.067,397.312V17.067H76.8v179.2  c0,2.645,1.28,5.205,3.413,6.827l34.133,25.6c1.451,1.109,3.243,1.707,5.12,1.707h273.067c1.877,0,3.669-0.597,5.12-1.707  l3.925-2.901l30.208-22.699c2.133-1.621,3.413-4.181,3.413-6.827v-179.2h59.733V494.933z"/>
+<path style="fill:#2B339B;" d="M435.2,17.067v179.2c0,2.645-1.28,5.205-3.413,6.827l-30.208,22.613v0.085l-3.925,2.901  c-1.451,1.109-3.243,1.707-5.12,1.707H119.467c-1.877,0-3.669-0.597-5.12-1.707l-34.133-25.6c-2.133-1.621-3.413-4.181-3.413-6.827  v-179.2H17.067v380.245l105.728,97.621h346.539V17.067H435.2z M384,443.733c0,1.707-0.512,3.328-1.451,4.693l-17.067,25.6  c-1.536,2.389-4.267,3.84-7.083,3.84H153.6c-2.816,0-5.547-1.451-7.083-3.84l-17.067-25.6c-0.939-1.365-1.451-2.987-1.451-4.693  v-93.867c0-1.707,0.512-3.328,1.451-4.693l17.067-25.6c1.536-2.389,4.267-3.84,7.083-3.84h204.8c2.816,0,5.547,1.451,7.083,3.84  l17.067,25.6c0.939,1.365,1.451,2.987,1.451,4.693V443.733z"/>
+<rect x="469.333" y="17.067" style="fill:#24067F;" width="25.6" height="477.867"/>
+<polygon style="fill:#7E939E;" points="418.133,17.067 418.133,192 401.835,204.288 403.883,34.133 93.867,34.133 93.867,17.067 "/>
+<polygon style="fill:#AFAFAF;" points="403.883,34.133 401.835,204.288 389.717,213.333 122.283,213.333 93.867,192 93.867,34.133   "/>
+<path d="M204.8,366.933v59.733c0,4.693-3.84,8.533-8.533,8.533s-8.533-3.84-8.533-8.533v-59.733c0-4.693,3.84-8.533,8.533-8.533  S204.8,362.24,204.8,366.933z"/>
+<path d="M382.549,345.173l-17.067-25.6c-1.536-2.389-4.267-3.84-7.083-3.84H153.6c-2.816,0-5.547,1.451-7.083,3.84l-17.067,25.6  c-0.939,1.365-1.451,2.987-1.451,4.693v93.867c0,1.707,0.512,3.328,1.451,4.693l17.067,25.6c1.536,2.389,4.267,3.84,7.083,3.84  h204.8c2.816,0,5.547-1.451,7.083-3.84l17.067-25.6c0.939-1.365,1.451-2.987,1.451-4.693v-93.867  C384,348.16,383.488,346.539,382.549,345.173z M366.933,441.173L353.792,460.8H158.208l-13.141-19.627v-88.747l13.141-19.627  h195.584l13.141,19.627V441.173z"/>
+<path style="fill:#F9F9F9;" d="M158.208,332.8l-13.141,19.627v88.747l13.141,19.627h183.125v-128H158.208z M204.8,426.667  c0,4.693-3.84,8.533-8.533,8.533s-8.533-3.84-8.533-8.533v-59.733c0-4.693,3.84-8.533,8.533-8.533s8.533,3.84,8.533,8.533V426.667z"/>
+<polygon style="fill:#E5E5E5;" points="366.933,352.427 366.933,441.173 353.792,460.8 341.333,460.8 341.333,332.8 353.792,332.8   "/>
+</svg>`
+
+
+const svgpencilicon = `<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg fill="#fff" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 width="20px" height="20px" viewBox="0 0 27.416 27.416"
+	 xml:space="preserve">
+<g>
+	<path d="M7.92,14.371l4.887,4.944l-8.004,3.021L7.92,14.371z M27.332,5.538v20.336c0,0.829-0.672,1.5-1.5,1.5H1.5
+		c-0.828,0-1.5-0.671-1.5-1.5V1.542c0-0.829,0.672-1.5,1.5-1.5h24.332c0.828,0,1.5,0.671,1.5,1.5v3.831l0.084,0.085L27.332,5.538z
+		 M24.332,8.39L13.684,18.513l-2.102-2.131l12.75-12.121V3.042h-0.251L10.84,15.63l-2.104-2.127L19.74,3.042H3v21.332h21.332V8.39z"
+		/>
+</g>
+</svg>`
+
+const svgdeleteicon = `<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M10.3094 2.25002H13.6908C13.9072 2.24988 14.0957 2.24976 14.2737 2.27819C14.977 2.39049 15.5856 2.82915 15.9146 3.46084C15.9978 3.62073 16.0573 3.79961 16.1256 4.00494L16.2373 4.33984C16.2562 4.39653 16.2616 4.41258 16.2661 4.42522C16.4413 4.90933 16.8953 5.23659 17.4099 5.24964C17.4235 5.24998 17.44 5.25004 17.5001 5.25004H20.5001C20.9143 5.25004 21.2501 5.58582 21.2501 6.00004C21.2501 6.41425 20.9143 6.75004 20.5001 6.75004H3.5C3.08579 6.75004 2.75 6.41425 2.75 6.00004C2.75 5.58582 3.08579 5.25004 3.5 5.25004H6.50008C6.56013 5.25004 6.5767 5.24998 6.59023 5.24964C7.10488 5.23659 7.55891 4.90936 7.73402 4.42524C7.73863 4.41251 7.74392 4.39681 7.76291 4.33984L7.87452 4.00496C7.94281 3.79964 8.00233 3.62073 8.08559 3.46084C8.41453 2.82915 9.02313 2.39049 9.72643 2.27819C9.90445 2.24976 10.093 2.24988 10.3094 2.25002ZM9.00815 5.25004C9.05966 5.14902 9.10531 5.04404 9.14458 4.93548C9.1565 4.90251 9.1682 4.86742 9.18322 4.82234L9.28302 4.52292C9.37419 4.24941 9.39519 4.19363 9.41601 4.15364C9.52566 3.94307 9.72853 3.79686 9.96296 3.75942C10.0075 3.75231 10.067 3.75004 10.3553 3.75004H13.6448C13.9331 3.75004 13.9927 3.75231 14.0372 3.75942C14.2716 3.79686 14.4745 3.94307 14.5842 4.15364C14.605 4.19363 14.626 4.2494 14.7171 4.52292L14.8169 4.82216L14.8556 4.9355C14.8949 5.04405 14.9405 5.14902 14.992 5.25004H9.00815Z"fill="#fff"/>
+<path d="M5.91509 8.45015C5.88754 8.03685 5.53016 7.72415 5.11686 7.7517C4.70357 7.77925 4.39086 8.13663 4.41841 8.54993L4.88186 15.5017C4.96736 16.7844 5.03642 17.8205 5.19839 18.6336C5.36679 19.4789 5.65321 20.185 6.2448 20.7385C6.8364 21.2919 7.55995 21.5308 8.4146 21.6425C9.23662 21.7501 10.275 21.7501 11.5606 21.75H12.4395C13.7251 21.7501 14.7635 21.7501 15.5856 21.6425C16.4402 21.5308 17.1638 21.2919 17.7554 20.7385C18.347 20.185 18.6334 19.4789 18.8018 18.6336C18.9638 17.8206 19.0328 16.7844 19.1183 15.5017L19.5818 8.54993C19.6093 8.13663 19.2966 7.77925 18.8833 7.7517C18.47 7.72415 18.1126 8.03685 18.0851 8.45015L17.6251 15.3493C17.5353 16.6971 17.4713 17.6349 17.3307 18.3406C17.1943 19.025 17.004 19.3873 16.7306 19.6431C16.4572 19.8989 16.083 20.0647 15.391 20.1552C14.6776 20.2485 13.7376 20.25 12.3868 20.25H11.6134C10.2626 20.25 9.32255 20.2485 8.60915 20.1552C7.91715 20.0647 7.54299 19.8989 7.26958 19.6431C6.99617 19.3873 6.80583 19.025 6.66948 18.3406C6.52892 17.6349 6.46489 16.6971 6.37503 15.3493L5.91509 8.45015Z"fill="#fff"/>
+<path d="M9.42546 10.2538C9.83762 10.2125 10.2052 10.5133 10.2464 10.9254L10.7464 15.9254C10.7876 16.3376 10.4869 16.7051 10.0747 16.7463C9.66256 16.7875 9.29503 16.4868 9.25381 16.0747L8.75381 11.0747C8.7126 10.6625 9.01331 10.295 9.42546 10.2538Z"fill="#fff"/>
+<path d="M14.5747 10.2538C14.9869 10.295 15.2876 10.6625 15.2464 11.0747L14.7464 16.0747C14.7052 16.4868 14.3376 16.7875 13.9255 16.7463C13.5133 16.7051 13.2126 16.3376 13.2538 15.9254L13.7538 10.9254C13.795 10.5133 14.1626 10.2125 14.5747 10.2538Z"fill="#fff"/>
+</svg>` 
+
+const getFavorites = () => {
+    const favorites = window.localStorage.getItem('favorites')
+    return JSON.parse(favorites || '[]') 
+}
+
+const addFavorite = (value) => {
+    const favorites = getFavorites()
+    favorites.push(value)
+
+    console.log('favorites', favorites)
+    window.localStorage.setItem('favorites', JSON.stringify(favorites));
+}
+
+// ПРоверка на существование поиска в избранном
+const saveFavoriteSearch = (name) => {
+        const favorites = getFavorites()
+
+        const targetName = name.trim();
+        const isExist = favorites.length && favorites.some((item) => item.name.toLowerCase() === targetName.toLowerCase());
+        if(isExist){
+            return 0
+        } else {
+            addFavorite({name, link: window.location.href})
+           return 1
+        }
+
+}
+
+const createFavoriteItem = (item) => {
+    const deleteFavoriteItem = document.createElement('button');
+
+    const deleteFavoriteHandler= (parent) => {
+        const favorites = getFavorites()
+      
+        const newFavorites = favorites.filter(favorite => favorite.name !== item.name)
+
+        window.localStorage.setItem('favorites', JSON.stringify(newFavorites));
+        parent.remove();
+    }
+
+    const editFavoriteHandler = () => {
+        console.log('редактировать');
+    }
+
+    deleteFavoriteItem.className = 'favoriteItemAction';
+    deleteFavoriteItem.title = 'удалить';
+    deleteFavoriteItem.innerHTML = svgdeleteicon;
+    deleteFavoriteItem.addEventListener('click', (event) => {
+        deleteFavoriteHandler(event.target.closest('.favoriteItem'))
+    });
+
+    // КНОПКА РЕДАКТИРОВАНИЯ ФИЛЬТРА
+    const editFavoriteItem = document.createElement('button');
+    editFavoriteItem.className = 'favoriteItemAction';
+    editFavoriteItem.title = 'редактировать';
+    editFavoriteItem.innerHTML = svgpencilicon;
+    editFavoriteItem.addEventListener('click', (event) => {
+        editFavoriteHandler()
+    });
+
+    // САМ ЭЛЕМЕНТ ФИЛЬТРа
+    const favoriteItem = document.createElement('div');
+    favoriteItem.className = 'favoriteItem';
+    favoriteItem.innerHTML = `
+      <a href="${item.link}" class="favoriteItemLink">
+        <span class="favoriteItemIcon">${item?.icon || 'i'}</span>
+        ${item.name}
+      </a>
+        <div class="favoriteItemActions"></div>`;
+
+       const actions = favoriteItem.querySelector('.favoriteItemActions');
+       actions.append(editFavoriteItem);
+       actions.append(deleteFavoriteItem);
+
+    return favoriteItem;
+};
+
+
+
+const addControls = (panel,) => {    
+
+    const favoritesListModal = document.createElement('div');
+
+
+    favoritesListModal.className = 'favoritesListModal';
+    favoritesListModal.innerHTML = `
+        <div class="favoritesListModalWrap">
+            <div class="favoritesListContent">
+                <h3 class="favoritesListTitle">Избранные фильтры</h3>
+                <div class="favoritesListItems"></div>
+            </div>
+            <div class="favoritesInfoText">Что бы открыть в новой вкладке, кликай на ссылку с зажатым ctrl(или cmd)</div>
+        </div>
+    `;
+
+
+
+    const favoritesListItems = favoritesListModal.querySelector('.favoritesListItems');
+
+    const removeModal = () => {
+        favoritesListModal.style.display = 'none';
+        favoritesListItems.innerHTML = ''; 
+    }
+
+    // СООБЩЕНИЕ О ОТСУТСТВИИ ФИЛЬТРОВ
+    const favoritesEmptyMessage = document.createElement('div');
+    favoritesEmptyMessage.className = 'empty';
+    favoritesEmptyMessage.innerHTML = 'тут пока ничего нет но ты можешь сохранить текущий (сокро сделаю)';
+
+    favoritesListModal.addEventListener('click', (event) => {
+        if(event.target.classList.contains('favoritesListModal')){
+            removeModal()
+        }
+    });
+
+   
+
+    const openFavoriteModal = () => {
+        const favorites = getFavorites()
+        favoritesListModal.style.display = 'flex';
+        favoritesListItems.innerHTML = ''; 
+        if(favorites?.length){
+            //добавляем список избранных фильтров
+             favorites.forEach(item => {
+                favoritesListItems.append(createFavoriteItem(item))
+            });
+        }else{
+            //добавляем сообщение о том что ничего нет
+            favoritesListItems.append(favoritesEmptyMessage)
+        }
+    }
+
+    // ПОПАП СОХРАНЕНИЯ ФИЛЬТРА
+    const savePopup = document.createElement('div');
+    savePopup.className = 'savePopupOverlay';
+    savePopup.innerHTML = `
+    <div class="savePopupContent">
+        <h3>Сохранить фильтр</h3>
+        <div class="savePopupField"></div>
+    </div>
+    `;
+
+    
+    // КНОПКА СОХРАНЕНИЯ ФИЛЬТРА 
+    const saveFavoritesButton = document.createElement('button');
+    saveFavoritesButton.className = 'savePopupBtn';
+    saveFavoritesButton.disabled = true;
+    saveFavoritesButton.textContent = 'Сохранить';
+
+    
+    // ИНПУТ ДЛЯ НАЗВАНИЯ ФИЛЬТРА
+    const savePopupInput = document.createElement('input');
+    savePopupInput.className = 'savePopupInput';
+    savePopupInput.placeholder = 'Название поиска';
+    savePopupInput.type = 'text';
+    savePopupInput.addEventListener('input', (event) => {
+        if(event.target.value){
+            saveFavoritesButton.disabled = false;
+        }else{
+            saveFavoritesButton.disabled = true;
+        }
+    });
+
+    saveFavoritesButton.addEventListener('click', async () => {
+        const result =  saveFavoriteSearch(savePopupInput.value);
+
+        if(result){
+            console.log('сохранено')
+            removePopup()
+        } else {
+            savePopupAlert.style.display = 'block';
+        }
+
+    });
+
+    // АЛЕРТ ЕСЛИ ПОИСК УЖЕ СУЩЕСТВУЕТ
+    const savePopupAlert = document.createElement('div');
+    savePopupAlert.className = 'savePopupAlert';
+    savePopupAlert.innerHTML = `<span>такое имя уже существует, заменяю его? </span>`;
+    
+    // ФУНКЦИЯ ЗАКРЫТИЯ ПОПАПА
+    const removePopup = () => {
+        savePopupInput.value = '';
+        savePopup.style.display = 'none';
+    }
+
+    // КНОПКА ЗАМЕНЫ ПОИСКА
+    const replaceButton = document.createElement('button');
+    replaceButton.className = 'replacePopupBtn';
+    replaceButton.textContent = 'Заменить';
+    replaceButton.addEventListener('click', () => {
+        const targetName = savePopupInput.value.trim();
+        addFavorite({name: targetName, link: window.location.href})
+        removePopup()
+    });
+
+    savePopupAlert.appendChild(replaceButton);
+
+
+
+    const savePopupField = savePopup.querySelector('.savePopupField');
+    savePopupField.innerHTML = `
+            <select name="savePopupSelect" id="savePopupSelect">
+                <option value="1" selected>Вариант 1</option>
+                <option value="2">Вариант 2</option>
+                <option value="3">Вариант 3</option>
+            </select>
+    `;
+    savePopupField.append(savePopupInput, saveFavoritesButton);
+
+    const savePopupContent = savePopup.querySelector('.savePopupContent');
+    savePopupContent.append(savePopupAlert);
+
+    
+    savePopup.addEventListener('click', (event) => {
+        if(event.target.classList.contains('savePopupOverlay')){
+            removePopup()
+        }
+    });
+
+
+
+
+    // КОНЕЦ ПОПАПА СОХРАНЕНИЯ ПОИСКА
+    document.body.appendChild(favoritesListModal);
+    document.body.appendChild(savePopup);
+
+    window.addEventListener('keydown', (event) => {
+        console.log('event.key', event.key)
+        if(event.key === 'Escape'){
+            removePopup();
+            removeModal()
+        }
+    });
+
+    /**
+     * CONTROLS
+     */
+    const controls = panel.querySelector('.controls');
+    const center = controls.querySelector('.controls-center');
+    const input = center.querySelector('input.multiselect__input');
+    const submitButton = center.querySelector('button.search-btn');
+    document.addEventListener('keyup', (event) => {
+        // 13 = enter
+        if(event.key === 13){
+            if(input.value.length ){
+                submitButton.click();
+            }
+        }
+    });
+
+    //кнопка сохранения на панели
+    const saveButton = document.createElement('button');
+    saveButton.className = 'saveBtn';
+    saveButton.innerHTML = svgsaveicon;
+    saveButton.addEventListener('click', () => {
+        savePopup.style.display = 'flex';
+    });
+    saveButton.setAttribute('title', 'Сохранить фильтр');
+
+    controls.appendChild(saveButton);
+
+    //кнопка избранного на паалени
+    const favoritesButton = document.createElement('button');
+
+    favoritesButton.className = 'favoritesBtn';
+    favoritesButton.innerHTML = 'Favorites';
+    favoritesButton.addEventListener('click', () => {
+        openFavoriteModal();
+    });
+
+
+    controls.appendChild(favoritesButton);
+
+}
+ 
+
+
+    const inputPanelInit = (panel)=> {
+        addControls(panel);
+
+    }
+
+
+    
+
+
+    const waitForElement = (selector, { intervalMs = 300, timeoutMs = 10_000 } = {}) =>
+        new Promise((resolve) => {
+            const startedAt = Date.now();
+
+            const tick = () => {
+                const el = document.querySelector(selector);
+                if (el) return resolve(el);
+                if (Date.now() - startedAt >= timeoutMs) return resolve(null);
+                setTimeout(tick, intervalMs);
+            };
+
+            tick();
+        });
+
+    (async () => {
+        const panelElement = await waitForElement('.search-panel', { intervalMs: 300, timeoutMs: 10_000 });
+        if (panelElement) inputPanelInit(panelElement);
+    })();
